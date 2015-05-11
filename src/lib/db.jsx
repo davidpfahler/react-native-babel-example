f99819b224cb6caf2a9e1627fb9d144db6071b70
David Pfahler <david@excellenteasy.com>
2015-05-11 14:13:53 +0200

    WIP: try to add atob/btoa functions

--- src/lib/db.jsx
+++ src/lib/db.jsx
@@ -2,6 +2,10 @@
 
 import PouchDB from 'pouchdb'
 import asyncadapter from 'pouchdb-async-storage'
+import Base64 from 'Base64'
+
+global.atob = Base64.atob
+global.btoa = Base64.btoa
 
 let db = new PouchDB('exampledb', {adapter: 'asyncstorage'})
 global.db = db
